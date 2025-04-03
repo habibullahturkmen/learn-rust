1d2d510c718c7dd954e4277e3199417ba5572ed4
Habibullah Turkmen <habibullah_turkmen@yahoo.com>
2025-04-03 16:18:15 +0430

    init and intro exercises done

---
+++ Common Programming Concepts/Variables/Intro/src/main.rs
@@ -0,0 +1,8 @@
+fn main() {
+    // put your code here to launch it
+    print!("{}", add_one(22));
+}
+
+fn add_one(x: i32) -> i32 {
+    x + 1
+}
---
+++ Common Programming Concepts/Variables/Introduce Variable Refactoring/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    const NINETEEN_NINETY_THREE: i32 = 1993;
+    let ten_years_ago = NINETEEN_NINETY_THREE - 10;
+    let five_years_ago = NINETEEN_NINETY_THREE - 5;
+    let one_year_ago = NINETEEN_NINETY_THREE - 1;
+
+    println!("Ten years ago: {}", ten_years_ago);
+    println!("Five years ago: {}", five_years_ago);
+    println!("One year ago: {}", one_year_ago);
+}
